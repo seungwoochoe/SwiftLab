aac23dcb88d13c722f28e2b60add69514651d4e6
Seungwoo Choe <seungwoochoes@gmail.com>
2023-02-11 07:43:13 +0900

    Initial Commit

---
+++ SwiftLab/SwiftLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftLabApp.swift
+//  SwiftLab
+//
+//  Created by Seungwoo Choe on 2023-02-11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
